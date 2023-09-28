5c890ce867a0e34e6cab2ee61ae8dd12bfa59961
fajartc02 <fajartc02@gmail.com>
2023-09-28 23:19:33 +0700

    update login handler, add screen user master, fixing bug request API

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -10,17 +10,27 @@ import 'vuetify/styles'
 
 // Composables
 import { createVuetify } from 'vuetify'
+import {
+    VDataTable,
+    VDataTableServer,
+    VDataTableVirtual,
+} from "vuetify/labs/VDataTable";
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
-  theme: {
-    themes: {
-      light: {
-        colors: {
-          primary: '#1867C0',
-          secondary: '#5CBBF6',
+    theme: {
+        themes: {
+            light: {
+                colors: {
+                    primary: '#1867C0',
+                    secondary: '#5CBBF6',
+                },
+            },
         },
-      },
     },
-  },
+    components: {
+        VDataTable,
+        VDataTableServer,
+        VDataTableVirtual
+    }
 })
--- src/store/auth.module.js
+++ src/store/auth.module.js
@@ -2,41 +2,42 @@ import { toast } from "vue-sonner";
 import axios from "axios";
 
 export default {
-  state: {
-    userData: null,
-  },
-  mutations: {
-    setUserData(state, payload) {
-      state.userData = payload;
+    state: {
+        userData: null,
     },
-  },
-  getters: {
-    userData: (state) => {
-      return state.userData;
+    mutations: {
+        setUserData(state, payload) {
+            state.userData = payload;
+        },
     },
-  },
-  actions: {
-    LOGIN(context, payload) {
-      axios
-        .post("http://localhost:7000/api/v1/login", payload)
-        .then((res) => {
-          if (res.data.message == "success login") {
-            toast.success("Login success", {
-              duration: 500,
-            });
-            context.commit("setUserData", res.data.data);
-            localStorage.setItem("token", res.data.data);
-            window.location.href = "/";
-          }
-        })
-        .catch((e) => {
-          console.log(e);
-        });
+    getters: {
+        userData: (state) => {
+            return state.userData;
+        },
     },
-    LOGOUT(context) {
-      context.commit("setUserData", null);
-      localStorage.removeItem("token");
-      window.location.href = "/auth/login";
+    actions: {
+        LOGIN(context, payload) {
+            axios
+                .post("http://localhost:7000/api/v1/login", payload)
+                .then((res) => {
+                    if (res.data.message == "success login") {
+                        toast.success("Login success", {
+                            duration: 500,
+                        });
+                        context.commit("setUserData", res.data.data.user);
+                        localStorage.setItem("user", res.data.data.user.user_nm)
+                        localStorage.setItem("token", res.data.data.token);
+                        window.location.href = "/";
+                    }
+                })
+                .catch((e) => {
+                    console.log(e);
+                });
+        },
+        LOGOUT(context) {
+            context.commit("setUserData", null);
+            localStorage.removeItem("token");
+            window.location.href = "/auth/login";
+        },
     },
-  },
 };
